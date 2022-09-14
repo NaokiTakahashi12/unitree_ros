a82ff1456fd1a05a7334684ef53e90469c8e7154
NaokiTakahashi <naoki.takahashi060@gmail.com>
2022-09-14 22:43:02 +0900

    add controller_manager

--- robots/go1_description/launch/unitree_go1.launch.py
+++ robots/go1_description/launch/unitree_go1.launch.py
@@ -55,6 +55,27 @@ def generate_declare_launch_arguments():
             description = 'Use simulation time (boolean)'
         ),
         launch.actions.DeclareLaunchArgument(
+            'ros2_control_config_file',
+            default_value = [
+                os.path.join(
+                    this_pkg_share_dir,
+                    'config',
+                    'ros2_controllers.yaml'
+                )
+            ],
+            description = 'ros2 controller config file path (string)'
+        ),
+        launch.actions.DeclareLaunchArgument(
+            'use_real_hardware',
+            default_value = ['true'],
+            description = 'Using real hardware control (boolean)',
+        ),
+        launch.actions.DeclareLaunchArgument(
+            'ignition_gazebo',
+            default_value = ['false'],
+            description = 'Using ignition gazebo (boolean)',
+        ),
+        launch.actions.DeclareLaunchArgument(
             'use_rviz',
             default_value = ['false'],
             description = 'Using rviz2 (boolean)'
@@ -92,6 +113,13 @@ def generate_launch_nodes():
     robot_description = {
         'robot_description': launch.substitutions.Command([
             'xacro ',
+            ' use_real_hardware:=',
+            launch.substitutions.LaunchConfiguration('use_real_hardware'),
+            ' ignition_gazebo:=',
+            launch.substitutions.LaunchConfiguration('ignition_gazebo'),
+            ' ros2_control_config_file:=',
+            launch.substitutions.LaunchConfiguration('ros2_control_config_file'),
+            ' ',
             urdf_file
         ])
     }
@@ -130,6 +158,44 @@ def generate_launch_nodes():
                 on_exit = exit_event
             ),
             launch_ros.actions.Node(
+                package = 'controller_manager',
+                executable = 'ros2_control_node',
+                output = output,
+                parameters = [
+                    use_sim_time,
+                    robot_description,
+                    launch.substitutions.LaunchConfiguration(
+                        'ros2_control_config_file'
+                    )
+                ],
+                remappings = [
+                    ('joint_states', 'joint_state_broadcaster/joint_states')
+                ],
+                condition = launch.conditions.UnlessCondition(
+                    launch.substitutions.LaunchConfiguration('ignition_gazebo')
+                )
+            ),
+            launch_ros.actions.Node(
+                package = 'controller_manager',
+                executable = 'spawner',
+                output = output,
+                arguments = [
+                    'joint_state_broadcaster',
+                    '--controller-manager',
+                    'controller_manager'
+                ]
+            ),
+            launch_ros.actions.Node(
+                package = 'controller_manager',
+                executable = 'spawner',
+                output = output,
+                arguments = [
+                    'joint_trajectory_controller',
+                    '--controller-manager',
+                    'controller_manager'
+                ]
+            ),
+            launch_ros.actions.Node(
                 package = 'rviz2',
                 executable = 'rviz2',
                 name = 'rviz2',
@@ -148,7 +214,7 @@ def generate_launch_nodes():
                         'use_rviz'
                     )
                 )
-            ),
+            )
         ])
     ]
 
